64ce32c9997e229375d61b342bd19e57893d86d8
steelberg <143072293+mayakovskiyy@users.noreply.github.com>
2025-09-04 23:29:01 +0300

    Actual 0.0.1.1

--- main.rs
+++ main.rs
@@ -1,9 +1,7 @@
 use rand::prelude::*;
 
 fn main() {
-    
-    // needs to optimize ↓↓↓
-    let letters = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '!', '?', '/', '@', '#', '$', '%', ':', '^', '&', '*', '(', ')', '_', '-', '+', '=', '~', '`', ';'];
+    let letters = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '!', '?', '/', '@', '#', '$', '%', ':', '^', '&', '*', '(', ')', '_', '-', '+', '=', '~', '`'];
     let mut char1: u8 = rand::random();
     let mut char2: u8 = rand::random();
     let mut char3: u8 = rand::random();
@@ -12,14 +10,12 @@ fn main() {
     let mut char6: u8 = rand::random();
     let mut char7: u8 = rand::random();
     let mut char8: u8 = rand::random();
-    let mut char9: u8 = rand::random();
     let mut num2: u8 = rand::random();
     let mut num3: u8 = rand::random();
     let mut num4: u8 = rand::random();
-    
+    let mut char9: u8 = rand::random();
     loop{
-        if char1 > 71 || char2 > 71 || char3 > 71 || char4 > 71 || char5 > 71 || char6 > 71 || char7 > 71 || char8 > 71 || char9 > 71 {
-        // needs to optimize ↓↓↓
+        if char1 > 70 || char2 > 70 || char3 > 70 || char4 > 70 || char5 > 70 || char6 > 70 || char7 > 70 || char8 > 70 || char9 > 70 {
             char1 = rand::random();
             char2 = rand::random();
             char3 = rand::random();
@@ -34,7 +30,7 @@ fn main() {
             num4 = rand::random();
         }
         else {
-            println!("{}{}{}{}{}{}{}{}{}{}{}{}",
+            println!("BarentszPassGen [SUCCESFUL]: {}{}{}{}{}{}{}{}{}{}{}{}",
                 letters[char1 as usize],
                 num2, letters[char2 as usize],
                 letters[char3 as usize],
